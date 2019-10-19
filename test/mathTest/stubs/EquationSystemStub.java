a4cfd36643dd13e4a71ad507b3fa2a81f0af7b72
225038 <zhurbovaa@mail.ru>
2019-10-19 21:08:06 +0300

    Small fixes

--- test/mathTest/stubs/EquationSystemStub.java
+++ test/mathTest/stubs/EquationSystemStub.java
@@ -1,12 +1,14 @@
 package mathTest.stubs;
 
+import math.EquationsSystem;
 import math.LogarithmeticalExpression;
 import math.TrigonometricExpression;
 
-public class EquationSystemStub {
+public class EquationSystemStub extends EquationsSystem {
     LogarithmeticalExpression logarithmeticalExpression = new LogarithmeticalExpressionStub();
     TrigonometricExpression trigonometricExpression = new TrigonometricExpressionStub();
 
+    @Override
     public double calculate(double x, double precision) {
         return x > 0 ? logarithmeticalExpression.calculate(x, precision) : trigonometricExpression.calculate(x, precision);
     }
